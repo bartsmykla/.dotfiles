af8e5c1c199f7ca899eac43f4eff335ce2b7959a
Bart Smykla <bartek@smykla.com>
2020-12-19 15:56:57 +0100

    [fish] add ctrl+space binding to accept sugge
    `ctrl + space` will now accept the suggestion:
    `bind -k nul accept-autosuggestion`
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 # autojump 
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
+# key bindings
+  # https://github.com/fish-shell/fish-shell/issues/3189
+  # nul is a name of `Ctrl + Space` key binding
+  bind -k nul accept-autosuggestion
