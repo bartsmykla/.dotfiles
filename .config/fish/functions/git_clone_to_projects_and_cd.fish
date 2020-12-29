abe5986590a523bb75a12dfb47d193188d9d5006
Bart Smykla <bartek@smykla.com>
2020-12-29 09:21:10 +0100

    [fish] add function "git_clone_to_projects_and_cd"
    - Function `git_clone_to_projects_and_cd` will create a directory for a
      repository which url is being provided as a first argument in a format
      `$PROJECTS_PATH/org_name/repo_name` and if the `org_name` directory
      won't exist it will also create it
    - Also created abbreviation for the function: `pgc` (projects git clone)
    
    Notes:
    - Currently function expects `$PROJECT_PATH` env var to be defined
      to work
    - Currently function only accepts the repo urls in format:
      `git:github.com/org_name/repo_name.git`
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

---
+++ .config/fish/functions/git_clone_to_projects_and_cd.fish
@@ -0,0 +1,32 @@
+function git_clone_to_projects_and_cd --description "Git clone repository to\
+ \$PROJECTS_PATH ($PROJECTS_PATH) and create parent directory if doesn't exist"\
+ --argument-names repo_url;
+  # TODO: Make it configurable
+  if ! set -q PROJECTS_PATH;
+    echo "Variable \$PROJECTS_PATH is not defined" >&2
+    return 1
+  end
+
+  set regex 's/^git@github\\.com:(.+)?\\/(.+)?.git$/\1 \2/'
+  set names (echo $repo_url | sed -E $regex | string split " ")
+
+  if test (count $names) -ne 2
+    echo "Invalid or unsupported repository path ($repo_url)" >&2
+    return 121
+  end
+
+  set org_name $names[1]
+  set repo_name $names[2]
+  set org_path $PROJECTS_PATH/$org_name
+  set full_path $org_path/$repo_name
+
+  # TODO: Add option to overwrite directories
+  if test -e $full_path
+    echo "Directory \"$full_path\" already exists" >&2
+    return 121
+  end
+
+  mkdir -p $full_path && \
+  git clone $repo_url $full_path && \
+  cd $full_path
+end
