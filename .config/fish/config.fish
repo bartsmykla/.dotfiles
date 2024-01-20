4285f3c9849b97a1822ebb818333281fd33c9777
Bart Smykla <bartek@smykla.com>
2024-01-20 07:56:45 +0100

    Remove Ctrl+Space key shortcut from config.fish
    As it was moved to `.config/fish/functions/fish_user_key_bindings.fish`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,11 +8,6 @@
 # autojump 
   [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
-# key bindings
-  # https://github.com/fish-shell/fish-shell/issues/3189
-  # nul is a name of `Ctrl + Space` key binding
-  bind -k nul forward-char
-
 # fzf
   export FZF_DEFAULT_OPTS="--bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all"
 
