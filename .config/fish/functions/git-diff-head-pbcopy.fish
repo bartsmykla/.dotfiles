c4c698332ce90b691ac8fc558e8d2c239deba85f
Bart Smykla <bartek@smykla.com>
2025-04-27 10:00:31 +0200

    feat(abbr): add diff (d/dfi) shortcuts for git diff with clipboard support
    Added new abbreviations `d` and `dfi` to diff changes against HEAD and copy results.
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

---
+++ .config/fish/functions/git-diff-head-pbcopy.fish
@@ -0,0 +1,5 @@
+function git-diff-head-pbcopy \
+  --description "Diff against HEAD and copy the output to clipboard"
+
+  af shortcuts abbreviations gd --reference HEAD --pbcopy
+end
---
+++ .config/fish/functions/git-diff-head.fish
@@ -0,0 +1,5 @@
+function git-diff-head \
+  --description "Diff against HEAD"
+
+  af shortcuts abbreviations gd --reference HEAD
+end
